cc9ef1e474e5b7c953e510eff8ca2258670fdd40
lindraaa <chancruz739@gmail.com>
2025-05-26 09:34:00 +0800

    create hashing and bcrypt service

---
+++ src/modules/users/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { HashingService } from './hashing/hashing.service';
+import { BcryptService } from './hashing/bcrypt.service';
+
+@Module({
+  // providers: [HashingService, BcryptService]
+  providers:[
+    {
+      provide:HashingService,
+      useClass:BcryptService,
+    },
+  ]
+  
+})
+export class AuthModule {}
