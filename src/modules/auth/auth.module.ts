30f89216b497824af71822369f270ecc9ec7831d
lindraaa <chancruz739@gmail.com>
2025-05-27 12:41:02 +0800

    jwt tokens

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,10 +5,18 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import jwtConfig from './config/jwt.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports:[TypeOrmModule.forFeature([User])],
+    imports:[
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig)
+],
     providers:[BcryptService, AuthService],
     controllers: [AuthController],
 })
 export class AuthModule {}
+ 
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 import { Repository } from 'typeorm';
@@ -6,28 +6,22 @@ import { SignUpDto } from './dto/sign-up.dto';
 import { BcryptService } from './hashing/bcrypt.service';
 import { createResponse } from 'src/shared/utils/response.util';
 import { SignInDto } from './dto/sign-in.dto';
+import { JwtService } from '@nestjs/jwt';
+import jwtConfig from './config/jwt.config';
+import { ConfigType } from '@nestjs/config';
+
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(User) private readonly usersRespository:Repository<User>,
         private readonly bcryptService:BcryptService,
+        private readonly jwtService:JwtService,
+        
+        @Inject(jwtConfig.KEY)//for env
+        private readonly jwtConfiguration:ConfigType<typeof jwtConfig> 
     ){}
-    async findUserByEmail(email:string):Promise<User|null>{
-        return this.usersRespository.findOne({where:{email}})
-    }
-    async validateUser(email:string,password:string):Promise<User>{
-        const user = await this.findUserByEmail(email)
-        if(!user) throw new UnauthorizedException("Invalid username or password");
-        
-        const isMatch = await this.bcryptService.compare(password,user.password)
-        if(!isMatch) throw new UnauthorizedException("Invalid username or password");
-        
-        return user;
-
-
-    }
-
+   
     async signUp(signUpDto:SignUpDto){
         const existingUser = await this.findUserByEmail(signUpDto.email)
         if(existingUser) throw new ConflictException("Email already exits");
@@ -41,7 +35,34 @@ export class AuthService {
     }
     async sigIn(signInDto:SignInDto){
         const user = await this.validateUser(signInDto.email,signInDto.password)
-        return createResponse("success",'login successfully',user);
+        const payloadToken = {
+            sub:user.id, 
+            email:user.email,
+            password:user.password
+        }
+        
+        const options = {
+            audience:this.jwtConfiguration.audience,
+            issuer:this.jwtConfiguration.issuer,
+            secret:this.jwtConfiguration.secret,
+            expiresIn:this.jwtConfiguration.accessTokenTTl
+        }
+        const res = await this.jwtService.signAsync(payloadToken,options)
+        return createResponse("success",'login successfully',{accessToken:res,user});
+
+    }
+    async findUserByEmail(email:string):Promise<User|null>{
+        return await this.usersRespository.findOne({where:{email}})
+    }
+    async validateUser(email:string,password:string):Promise<User>{
+        const user = await this.findUserByEmail(email)
+        if(!user) throw new UnauthorizedException("Invalid username or password");
+        
+        const isMatch = await this.bcryptService.compare(password,user.password)
+        if(!isMatch) throw new UnauthorizedException("Invalid username or password");
+        
+        return user;
+
 
     }
 }
