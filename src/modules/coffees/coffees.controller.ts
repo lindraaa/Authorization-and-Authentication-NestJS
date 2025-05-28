13601ff57d8ae943f1b85d7063ed66bc5ca6d696
lindraaa <chancruz739@gmail.com>
2025-05-28 20:33:50 +0800

    protected routes

--- src/modules/coffees/coffees.controller.ts
+++ src/modules/coffees/coffees.controller.ts
@@ -2,7 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { AuthType } from '../auth/enums/auth-type.enum';
 
+@Auth(AuthType.Bearer)
 @Controller('api/v1/coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
